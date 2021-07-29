6e1618062434899458e6051c968e766123b20ab2
wy <wy@wy.com>
2021-07-29 16:40:35 +0800

    add genKey

---
+++ genKey/genkey.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	// generate key
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// sk
+	privateKey := hex.EncodeToString(key.D.Bytes())
+	fmt.Println(privateKey)
+
+	// address
+	address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+	fmt.Println(address)
+}
