cba9f46297e955dce735d5d4eff1a857ea125f90
wy <wy@wy.com>
2021-08-03 14:48:47 +0800

    utils

--- sig/implement/utils/utils.go
+++ sig/implement/utils/utils.go
@@ -9,3 +9,10 @@ func Byte2Str(data []byte) string {
 	var str string = string(data[:len(data)])
 	return str
 }
+
+func MergeSlice(s1 []string, s2 []string) []string {
+	slice := make([]string, len(s1)+len(s2))
+	copy(slice, s1)
+	copy(slice[len(s1):], s2)
+	return slice
+}
